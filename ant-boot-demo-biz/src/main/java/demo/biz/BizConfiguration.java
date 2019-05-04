52c35712991a837ad78ba8ffc2ae429150c156e7
zhongxunking <zhongxunking@163.com>
2019-05-04 14:33:05 +0800

    引入idcenter

--- ant-boot-demo-biz/src/main/java/demo/biz/BizConfiguration.java
+++ ant-boot-demo-biz/src/main/java/demo/biz/BizConfiguration.java
@@ -8,11 +8,23 @@
  */
 package demo.biz;
 
+import org.antframework.common.util.id.Id;
+import org.antframework.idcenter.client.Ider;
+import org.antframework.idcenter.spring.IdersContexts;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * biz层配置
  */
 @Configuration
 public class BizConfiguration {
+
+    @PostConstruct
+    public void init() {
+        Ider ider = IdersContexts.getIder("uid");
+        Id id1 = ider.acquire();
+        Id id2 = ider.acquire();
+    }
 }
