905a50037124b1438b7e2b1d3bf4ce458781a0a6
zhongxunking <zhongxunking@163.com>
2019-10-13 17:11:49 +0800

    升级ant-boot版本到1.4.3.RELEASE

--- ant-boot-demo-biz/src/main/java/demo/biz/BizConfiguration.java
+++ ant-boot-demo-biz/src/main/java/demo/biz/BizConfiguration.java
@@ -8,27 +8,14 @@
  */
 package demo.biz;
 
-import org.antframework.common.util.id.Id;
-import org.antframework.idcenter.client.Ider;
-import org.antframework.idcenter.spring.IdersContexts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
 
 /**
  * biz层配置
  */
 @Configuration
 public class BizConfiguration {
-
-    @PostConstruct
-    public void init() {
-        Ider ider = IdersContexts.getIder("uid");
-        Id id1 = ider.acquire();
-        Id id2 = ider.acquire();
-    }
-
     @Value("${aa:${aa2:abc}}")
     private String aa;
 
